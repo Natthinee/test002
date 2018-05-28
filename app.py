0cc87b9834f7196af0411a1becbbacd592d1ed4f
khim ki khim <CHANLONN_N@hotmail.com>
2018-05-28 23:06:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,12 +112,12 @@ def bot():
     if text in evaluation_form['eval']['greet']:
          replyQueue.append(random.choice(evaluation_form['eval']['answer']))
     elif text in evaluation_form['eval']['ques'] or numberaa :
-             question = random.choice(evaluation_form['eval']['quest9'])
-             face = random.choice(evaluation_form['eval']['wordap'])
-             listanswer.append(question)
-             replyQueue.append(face+question)
-             replyQueue.append(setscoreq9['score']['pprint'])
-             replyQueue.append(please['ple']['ple'])        
+         question = random.choice(evaluation_form['eval']['quest9'])
+         face = random.choice(evaluation_form['eval']['wordap'])
+         listanswer.append(question)
+         replyQueue.append(face+question)
+         replyQueue.append(setscoreq9['score']['pprint'])
+         replyQueue.append(please['ple']['ple'])        
     else:
          replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
   ##########################################################################################################
