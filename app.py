a684dd49567e9ec94a7f7cae1d5af929412f832e
khim ki khim <CHANLONN_N@hotmail.com>
2018-05-28 23:11:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def bot():
   #########################################################################################################
     if text in evaluation_form['eval']['greet']:
          replyQueue.append(random.choice(evaluation_form['eval']['answer']))
-    elif text in evaluation_form['eval']['ques'] or numberaa :
+    elif text in evaluation_form['eval']['ques']:
          question = random.choice(evaluation_form['eval']['quest9'])
          face = random.choice(evaluation_form['eval']['wordap'])
          listanswer.append(question)
