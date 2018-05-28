9a530419bfad148a7661dba3e40ea0499f29c867
khim ki khim <CHANLONN_N@hotmail.com>
2018-05-28 23:17:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,6 @@
 from flask import Flask, request
 import json
 import requests
-import random
-listanswer = []
-question = ''
-face = ''
-score = 0
-sc = 0
-qe = ''
-faceqe = ''
-appe = []
-qqq2 =[]
-q2q = ''
-scc = 0
-i=0 
-questionnine = ''
-testttt = ''
-evaluation_form ={}
-numberaa = ['0','1','2','3']
-sayhi = open("sayhi.txt",encoding='utf-8-sig')
-sayhi = sayhi.read().split(',')
-answer = open("answer.txt","r",encoding='utf-8-sig')
-answer = answer.read().split(',')
-ques = open("Ques.txt","r",encoding='utf-8-sig')
-ques = ques.read().split(',')
-quest9 = open("Quest9.txt","r",encoding='utf-8-sig')
-quest9 = quest9.read().split(',')
-wordappende = open("wordappende.txt","r",encoding='utf-8-sig')
-wordappende = wordappende.read().split(',')
-qq2 = open("qq2.txt","r",encoding='utf-8-sig')
-qq2 = qq2.read().split(',')
-evaluation_form['eval'] ={'greet': sayhi,
-                         'answer': answer,
-                         'ques': ques,
-                         'quest9': quest9,
-                         'wordap': wordappende,
-                         'qq2': qq2,
-                         'number': number}
-answer0123 = {}
-answer0123['answer0123']={'answer0' : 'ไม่มีเลย',
-                         'answer1' : 'เป็นบางวัน',
-                         'answer2' : 'เป็นบ่อย',
-                         'answer3' : 'เป็นทุกวัน'}
-select2 = {}
-select2['selc'] = {'selc01': 'มี',
-                  'selc02' : 'ไม่มี'}
-                  
-please = {}
-please['ple'] = {'ple':'จิ้มที่เเป้นพิมพ์ 0 1 2 3 ตามระดับของอาการที่เป็นหน่อยน้าาา',
-                 'ple1':'พิมพ์ "มี" หน่อยนะถ้ามีอาการ พิมพ์ "ไม่มี" ถ้าไม่มีอาการน้าาา',
-                 'ple2' :'กรุณาตอบ "ไม่มี" ถ้าไม่มีอาการ หรือตอบ "มี" ถ้าไม่มีอาการ',
-                 'ple3': 'กรุณาตอบ "ได้" หากสามารถควบคุมอารมณ์ตัวเองได้ ตอบ "ไม่ไ้ด้" หากท่านไม่สามารถควบคุมอารมณ์ตนเองได้',
-                 'Error' : '-- เอ๊ะ! พิมพ์ผิดหรือเปล่าน้าาาา กอดอุ่นไม่เห็นเข้าใจเลย :/'}
-setscoreq9 = {}
-setscoreq9['score'] = {'pprint' : '0 = ไม่มีเลย\n' + '1 = เป็นบางวัน\n'+ '2 = เป็นบ่อย\n'+ '3 = เป็นทุกวัน\n'}
-quest8 = {}
-quest8['quest8'] = {'quest01':'มีความคิดอยากตาย หรือคิดว่าตายไปจะดีกว่า',
-       'quest02':'อยากทำร้ายตัวเอง หรือทำให้ตัวเองบาดเจ็บ',
-       'quest03':'มีความคิดเกี่ยวกับการฆ่าตัวตาย',
-       'quest031':'ท่านสามารถควบคุมความอยากฆ่าตัวตายที่ท่านคิดอยู่นั้นได้หรือไม่ หรือบอกได้ไหมว่าคงจะไม่ทำตามความคิดนั้นในขณะนี้',
-       'quest04':'มีแผนการที่จะฆ่าตัวตาย',
-       'quest05':'ได้เตรียมการที่จะทำร้ายตนเอง หรือเตรียมการจะฆ่าตัวตาย โดยตั้งใจว่าจะให้ตายจริงๆ',
-       'quest06':'ได้ทำให้ตนเองบาดเจ็บ แต่ไม่ตั้งใจที่จะทำให้เสียชีวิต',
-       'quest07':'ได้พยายามฆ่าตัวตาย โดยคาดหวัง/ตั้งใจที่จะให้ตาย',
-       'quest08':'ตลอดชีวิตที่ผ่านมา... ท่านเคยพยายามฆ่าตัวตาย'}
-ans8 = {}
-ans8['ans'] = {'an' :'ไม่ได้',
-               'ay' : 'ได้'}
 
 # ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
 global LINE_API_KEY
@@ -106,22 +40,11 @@ def bot():
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
     
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
-    
-  #########################################################################################################
-    if text in evaluation_form['eval']['greet']:
-         replyQueue.append(random.choice(evaluation_form['eval']['answer']))
-    elif text in evaluation_form['eval']['ques'] or numberaa :
-         question = random.choice(evaluation_form['eval']['quest9'])
-         face = random.choice(evaluation_form['eval']['wordap'])
-         listanswer.append(question)
-         replyQueue.append(face+question)
-         replyQueue.append(setscoreq9['score']['pprint'])
-         replyQueue.append(please['ple']['ple'])
-                
-    else:
-         replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
-  ##########################################################################################################
-  
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # if text in response_dict:
+    #     replyQueue.append(reponse_dict[text])
+    # else:
+    #     replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
        
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
     # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
@@ -132,9 +55,10 @@ def bot():
     # replyQueue.append(reponse_dict[closest])
    
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
-    #replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
-    #replyQueue.append(msg_in_string)
+    replyQueue.append(msg_in_string)
     reply(replyToken, replyQueue[:5])
     
     return 'OK', 200
@@ -160,4 +84,5 @@ def reply(replyToken, textList):
     return
 
 if __name__ == '__main__':
-    app.run()
+      app.run()
+
