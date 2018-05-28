ca2cebbeed4e7581b174b5887772ade1377d9be5
khim ki khim <CHANLONN_N@hotmail.com>
2018-05-29 00:10:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,14 +115,14 @@ def bot():
  
     if text in evaluation_form['eval']['greet']:
          replyQueue.append(random.choice(evaluation_form['eval']['answer']))
-    elif text in evaluation_form['eval']['ques'] or number :
+    elif text in evaluation_form['eval']['ques'] or evaluation_form['eval']['number'] :
          question = random.choice(evaluation_form['eval']['quest9'])
          face = random.choice(evaluation_form['eval']['wordap'])
          listanswer.append(question)
          replyQueue.append(face+question)
          replyQueue.append(setscoreq9['score']['pprint'])
          replyQueue.append(please['ple']['ple'])
-    elif text not in evaluation_form:
+    else:
          replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
 
   
