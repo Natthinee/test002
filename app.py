9e71f5b3f11230209a518ce0249f2792e890b47b
khim ki khim <CHANLONN_N@hotmail.com>
2018-05-29 01:08:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,14 @@ def bot():
          question = random.choice(evaluation_form['eval']['quest9'])
          face = random.choice(evaluation_form['eval']['wordap'])
          listanswer.append(question)
+         score = 1+1
          replyQueue.append(face+question)
          replyQueue.append(setscoreq9['score']['pprint'])
          replyQueue.append(please['ple']['ple'])
+         replyQueue.append(str(score))
+          
+         
+        
       
     else:
          replyQueue.append('กอดอุ่นงง')
