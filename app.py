d7686cc54e05e32ae8ca2907243ba237a217d768
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-02 18:09:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,13 @@ def bot():
     
     if text in evaluation_form['eval']['greet'] :
          replyQueue.append(random.choice(evaluation_form['eval']['answer'] ))
+    if text in evaluation_form['eval']['ques']  :
+         question = random.choice(evaluation_form['eval']['quest9'])
+         face = random.choice(evaluation_form['eval']['wordap'])
+         listanswer.append(question)
+         replyQueue.append(face+question)
+         replyQueue.append(setscoreq9['score']['pprint'])
+         replyQueue.append(please['ple']['ple'])
     else:
          replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
 
