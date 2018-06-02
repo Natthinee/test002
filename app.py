d3996e8fc721958f7cac2246e39517bee170f5a5
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-02 17:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,11 +110,11 @@ def bot():
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
 
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
-    response_dict = {'สวัสดี':'สวัสดีครับ'}
-    if text in response_dict:
+    
+    if text in 'สวัสดีจ้า':
         #with open('tes.txt', 'a') as data_write:
             #data_write.write(userID + "\n")
-        replyQueue.append(reponse_dict[text])
+        replyQueue.append(text)
     else:
          replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
 
