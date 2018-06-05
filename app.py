a18f0f0aec63227f91d32c2a13d6c12c7063a98d
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-06 00:47:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -238,38 +238,37 @@ def reply2(replyToken, textList):
     msgs = []
     for text in textList:
         msgs.append({
-           "type": "template",
-           "altText": "this is a image carousel template",
-           "template": {
-               "type": "image_carousel",
-               "columns": [
-                {
-                    "imageUrl": "https://example.com/bot/images/item1.jpg",
-                    "action": {
-                        "type": "postback",
-                        "label": "Buy",
-                        "data": "action=buy&itemid=111"
-                }
-                },
-                {
-                    "imageUrl": "https://example.com/bot/images/item2.jpg",
-                    "action": {
+             "type": "template",
+             "altText": "this is a image carousel template",
+             "template": {
+                 "type": "image_carousel",
+                 "columns": [
+                     {
+                       "imageUrl": "https://example.com/bot/images/item1.jpg",
+                       "action": {
+                         "type": "postback",
+                         "label": "Buy",
+                         "data": "action=buy&itemid=111" 
+                       }
+                    },
+                    {
+                      "imageUrl": "https://example.com/bot/images/item2.jpg",
+                      "action": {
                         "type": "message",
                         "label": "Yes",
                         "text": "yes"
-                  }
-                  },
-                  {
-                    "imageUrl": "https://example.com/bot/images/item3.jpg",
-                    "action": {
-                        "type": "uri",
-                        "label": "View detail",
-                        "uri": "http://example.com/page/222"
-            }
-          }
-      ]
-  }
-
+                      }
+                   },
+                   {
+                     "imageUrl": "https://example.com/bot/images/item3.jpg",
+                     "action": {
+                       "type": "uri",
+                       "label": "View detail",
+                       "uri": "http://example.com/page/222"
+                     }
+                   }
+                ]
+             }
         })
     data = json.dumps({
         "replyToken": replyToken,
